6abac1ba88b013bad3654eb3c567df6ab6ffa635
MreegendraNarayan <mreegendra2003211@gmail.com>
2025-05-06 10:02:05 +0530

    configured wsgi.py file for fixing waitress server usage

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 from app import app
 import os
+from waitress import serve
 
 application = app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    application.run(host="0.0.0.0", port=port)
+    print(f"Starting server on port {port}...")
+    serve(application, host="0.0.0.0", port=port)
