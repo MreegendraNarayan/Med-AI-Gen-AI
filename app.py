46e0d141261439b67e762700e3003bec6b9c80a2
MreegendraNarayan <mreegendra2003211@gmail.com>
2025-06-22 00:24:58 +0530

    changed port due to 502 gateway error

--- app.py
+++ app.py
@@ -111,6 +111,6 @@ def cleanup(response):
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     initialize_components()
     app.run(host="0.0.0.0", port=port)
